0a1f906cf36453f1f6c4d8794793c6864b87ac8d
William Dyce <wdyce@fastmail.com>
2025-01-24 22:00:32 +1100

    crab rotation

--- crab/crab.gd
+++ crab/crab.gd
@@ -4,13 +4,18 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
+	# Fall down
+	var onFloor : bool = is_on_floor()
+	if not onFloor:
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Handle jump
+	if Input.is_action_just_pressed("ui_accept") and onFloor:
 		velocity.y = JUMP_VELOCITY
+		
+	# Handle rotation
+	var input_rotation = Input.get_axis("crab_rotate_left", "crab_rotate_right")
+	rotate_y(input_rotation * delta)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
