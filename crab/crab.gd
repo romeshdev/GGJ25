52719c055f1fb3115b697aee3a28b9ad730db35e
William Dyce <wdyce@fastmail.com>
2025-01-24 22:03:22 +1100

    crab can only strafe

--- crab/crab.gd
+++ crab/crab.gd
@@ -19,8 +19,8 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_strafe = Input.get_axis("crab_strafe_left", "crab_strafe_right")
+	var direction = (transform.basis * Vector3(input_strafe, 0, 0)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
