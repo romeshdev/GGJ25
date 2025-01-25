9937d5db682472149e546955be1cd10a1c81bc38
William Dyce <wdyce@fastmail.com>
2025-01-25 17:41:04 +1100

    title screen

--- game/GameBehaviour.gd
+++ game/GameBehaviour.gd
@@ -8,5 +8,12 @@ func _ready():
 	assert(akimbo != null)
 
 func _process(delta):
+	# Send information from UI to crab
 	crab.input_rotation = akimbo.rotation_axis
 	crab.input_advance = akimbo.movement_axis
+	
+	# Exit game is requested
+	if Input.is_action_just_pressed("ui_cancel"):
+		var game = load("res://title/RootTitle.tscn").instantiate()
+		get_tree().root.add_child(game)
+		queue_free()
