5df9f8e4227c7c5884229b2c98e37ff0d6be2204
romesh <romesh.panditha@gmail.com>
2025-01-26 11:33:27 +1100

    adjustments to auto walk

--- crab/ik_target.gd
+++ crab/ik_target.gd
@@ -2,6 +2,7 @@ extends Marker3D
 
 @export var step_target: Node3D
 @export var step_distance: float = 10.0
+@export var fast_step_distance: float = 20.0
 
 @export var adjacent_target: Node3D
 @export var character: CharacterBody3D
@@ -13,15 +14,11 @@ func _ready():
 var is_stepping := false
 
 func _process(delta):
-	var velocity = character.global_position - previous_position
-	#global_position = character.global_position + velocity * offset
-	var thing = abs(previous_position.distance_to(character.global_position)) / 2
-	print("Step distance: " + str(step_distance).pad_decimals(2))
-	print("Velocity: " + str(velocity))
-	print("Proposed step: " + str(thing))
-	previous_position = character.global_position
+	var character_velocity = character.global_position - previous_position
+	var vel = sqrt(character_velocity.x*character_velocity.x + character_velocity.y*character_velocity.y + character_velocity.z*character_velocity.z)
+	var current_step_distance = step_distance if vel < 8 else (fast_step_distance)
 	
-	if !is_stepping && !adjacent_target.is_stepping && abs(global_position.distance_to(step_target.global_position)) > (step_distance):
+	if !is_stepping && !adjacent_target.is_stepping && abs(global_position.distance_to(step_target.global_position)) > (current_step_distance):
 		step()
 		
 func step():
@@ -32,3 +29,4 @@ func step():
 	t.tween_property(self, "global_position", half_way + owner.basis.y, 0.1)
 	t.tween_property(self, "global_position", target_pos, 0.1)
 	t.tween_callback(func(): is_stepping = false)
+	previous_position = character.global_position
