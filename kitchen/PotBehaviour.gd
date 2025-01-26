352525636cb47d9d07ab8add70d9fbd9f680e7ac
William Dyce <wdyce@fastmail.com>
2025-01-26 12:02:13 +1100

    Victory is now possible

--- kitchen/PotBehaviour.gd
+++ kitchen/PotBehaviour.gd
@@ -3,19 +3,31 @@ class_name PotBehaviour extends Node3D
 signal eggInPot
 signal crabInPot
 
+var gameEnding : bool = false
+var isDefeat : bool = false
+var timer : float = -1
+
 func _ready():
 	pass
 	
 func _process(delta):
-	pass
+	# Countdown timer
+	if timer < 0:
+		return
+	timer = timer - delta
+	if timer <= 0:
+		timer = -1
+		if isDefeat:
+			crabInPot.emit()
+		else:
+			eggInPot.emit()
 
 func _on_pot_water_body_entered(body):
 	if body is CrabBehaviour:
-		print("Crab in pot!")
-		crabInPot.emit()
+		isDefeat = true
+		timer = 2
 	if body is EggBehaviour:
-		print("Egg in pot!")
-		eggInPot.emit()
+		timer = 2
 
 func _on_pot_water_body_exited(body):
 	pass
