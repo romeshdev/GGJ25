984489e9ce50e3a51fc460154ac5c3aadc3e9881
William Dyce <wdyce@fastmail.com>
2025-01-26 11:24:05 +1100

    Egg can now be picked up

--- claws/ClawRotationBehaviour.gd
+++ claws/ClawRotationBehaviour.gd
@@ -1,6 +1,8 @@
 class_name ClawRotatorBehaviour extends Node3D
 
 @export var flip : bool = false
+@export var grabArea : Area3D
+@export var actionName : StringName
 
 const ROTATION_SPEED : float = 5.0
 const MIN_ANGLE : float = 0.0
@@ -9,6 +11,8 @@ const MIN_ANGLE_FLIPPED : float = PI*1.25
 const MAX_ANGLE_FLIPPED : float = 2*PI
 
 var target : Vector2
+var egg : EggBehaviour
+var eggIsGrabbed : bool
 
 func _normalizeAngle(angle) -> float:
 	var twopi = 2*PI
@@ -17,9 +21,11 @@ func _normalizeAngle(angle) -> float:
 	return fmod(angle, twopi)
 
 func _ready():
+	assert(grabArea != null)
 	target = Vector2.UP
 
 func _process(delta):
+	# Rotate claws
 	var angle : float = 0.0
 	if flip:
 		target.x = max(0.00001, target.x)
@@ -30,3 +36,32 @@ func _process(delta):
 		angle = _normalizeAngle(-PI/2 -target.angle())
 		angle = clamp(angle, MIN_ANGLE, MAX_ANGLE)
 	rotation.y = lerp_angle(rotation.y, angle, delta * ROTATION_SPEED) 
+	
+	# Grab shit
+	if Input.is_action_just_pressed(actionName) && egg != null:
+		eggIsGrabbed = true
+		egg.freeze = true
+		egg.linear_velocity = Vector3.ZERO
+		egg.angular_velocity = Vector3.ZERO
+		
+	# Drag shit
+	if egg != null && eggIsGrabbed:
+		egg.global_position = grabArea.global_position
+		
+	# Drop shit
+	if Input.is_action_just_released(actionName) && egg != null:
+		eggIsGrabbed = false
+		egg.freeze = false
+	
+
+func _on_area_3d_body_entered(body):
+	if eggIsGrabbed:
+		return
+	if body is EggBehaviour:
+		egg = body as EggBehaviour
+
+func _on_area_3d_body_exited(body):
+	if eggIsGrabbed:
+		return
+	if body == egg:
+		egg = null
